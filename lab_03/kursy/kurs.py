53b9841178696acfa56c9620bb8fda293404ced6
Konrad <38508706+KonuTech@users.noreply.github.com>
2023-04-22 17:24:10 +0200

    Add files via upload

---
+++ lab_03/kursy/kurs.py
@@ -0,0 +1,6 @@
+class Kurs:
+
+    def __init__(self, nazwa, tryb):
+        self.nazwa = nazwa
+        self.tryb = tryb
+        self.listaKursantow = []
---
+++ lab_03/zatrudnieni/pracownicy.py
@@ -0,0 +1,7 @@
+class Pracownik:
+
+    def __init__(self, imie: str, nazwisko: str, email: str, telefon: str) -> None:
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.email = email
+        self.telefon = telefon
