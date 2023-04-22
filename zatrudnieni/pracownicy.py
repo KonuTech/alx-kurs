553f77ccc8aaa607ed34140e90e626ac45ecfaa9
Konrad <38508706+KonuTech@users.noreply.github.com>
2023-04-22 12:41:47 +0200

    Delete zatrudnieni directory

--- zatrudnieni/pracownicy.py
+++
@@ -1,7 +0,0 @@
-class Pracownik:
-
-    def __init__(self, imie: str, nazwisko: str, email: str, telefon: str) -> None:
-        self.imie = imie
-        self.nazwisko = nazwisko
-        self.email = email
-        self.telefon = telefon
