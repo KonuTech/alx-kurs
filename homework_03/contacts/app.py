7fae977301949839cbe661586ddcc0bb59088c6d
KonuTech <borowiec.k@gmail.com>
2023-05-02 15:18:13 +0200

    hw 03

--- homework_03/contacts/app.py
+++ homework_03/contacts/app.py
@@ -28,7 +28,7 @@ def contacts():
             print(f"{tab}Empty list of Contacts. Please register a Contact first.{new_line}")
             return
 
-        contact_surname = input("\tInput the Surname of a Contact to be listed:\n")
+        contact_surname = input("\tInput a Surname of a Contact to be listed:\n")
         if not contact_surname:
             print("\tMissing Contact's Surname. Please pass a Contact's Surname.\n")
             return
@@ -37,12 +37,12 @@ def contacts():
             if note.surname == contact_surname.capitalize():
                 print(
                     f"""
-                    List of registered Courses:
-                    ID: {n + 1}
-                    NAME: {note.name}
-                    SURNAME: {note.surname}
-                    PHONES: {note.phones}
-                    EMAILS: {note.emails}
+                    Contact details:
+                        ID: {n + 1}
+                        NAME: {note.name}
+                        SURNAME: {note.surname}
+                        PHONES: {note.phones}
+                        EMAILS: {note.emails}
                     """
                 )
 
@@ -53,7 +53,7 @@ def contacts():
             print(f"{tab}Error: Contact's Name cannot be empty.{new_line}")
             return
 
-        surname = input(f"{tab}Input the Surname of a Contact:{new_line}")
+        surname = input(f"{tab}Input a Surname of a Contact:{new_line}")
         if not surname:
             print(f"{tab}Error: Contact's Surname cannot be empty.{new_line}")
             return
@@ -68,39 +68,134 @@ def contacts():
             print(f"{tab}Error: Contact's E-mail cannot be empty.{new_line}")
             return
 
-        friend = Contact(
+        person = Contact(
             name=name.capitalize(),
             surname=surname.capitalize(),
-            phones=phone,
-            emails=email
+            phones=[phone],
+            emails=[email]
         )
 
         for n, note in enumerate(notes):
-            if note.surname == friend.surname:
+            if note.surname == person.surname:
                 print(f"{tab}Error: Contact Surname already in a list of Contacts.{new_line}")
                 return
 
-        notes.append(friend)
+        notes.append(person)
         print(
             f"""
-            {tab}Contact {friend.name}-{friend.surname}-{friend.phones}-{friend.emails} added to the Clinics list.{new_line}
+            {tab}Contact {person.name}-{person.surname}-{person.phones}-{person.emails} added to the Clinics list.{new_line}
             """
         )
 
     def remove_contact(file_name, delimiter=DELIMITER, encoding=ENCODING):
-        pass
+        """
+        Prompts the user to provide a Surname of a Contact to be removed.
+        :return: None
+        """
+        if len(notes) == 0:
+            print(f"{tab}Empty list of Contacts. Please register a Contact first.{new_line}")
+            return
+
+        contact_surname = input("\tInput a Surname of a Contact to be listed:\n")
+        if not contact_surname:
+            print("\tMissing Contact's Surname. Please pass a Contact's Surname.\n")
+            return
+
+        for n, note in enumerate(notes):
+            if note.surname == contact_surname.capitalize():
+                notes.pop(n)
+                print(f"{tab}Contact {note.surname} removed from the list of registered Contacts.{new_line}")
+                print(
+                    f"""
+                    Contact {note.surname} removed:
+                        ID: {n + 1}
+                        NAME: {note.name}
+                        PHONES: {note.phones}
+                        EMAILS: {note.emails}
+                    """
+                )
 
     def add_phone(file_name, delimiter=DELIMITER, encoding=ENCODING):
-        pass
+
+        contact_surname = input(f"{tab}Input a Surname of a Contact:{new_line}")
+        if not contact_surname:
+            print(f"{tab}Error: Contact's Surname cannot be empty.{new_line}")
+            return
+
+        phone = input(f"{tab}Input a Phone number of a Contact:{new_line}")
+        if not phone:
+            print(f"{tab}Error: Contact's Phone number cannot be empty.{new_line}")
+            return
+
+        for n, note in enumerate(notes):
+            if note.surname == contact_surname.capitalize():
+                person = Contact(
+                    name=note.name,
+                    surname=note.surname,
+                    phones=note.phones,
+                    emails=note.emails
+                )
+
+                person.phones.append(phone)
 
     def remove_phone(file_name, delimiter=DELIMITER, encoding=ENCODING):
-        pass
+
+        contact_surname = input(f"{tab}Input a Surname of a Contact:{new_line}")
+        if not contact_surname:
+            print(f"{tab}Error: Contact's Surname cannot be empty.{new_line}")
+            return
+
+        contact_phone = input(f"{tab}Input a Phone number of a Contact to be removed:{new_line}")
+        if not contact_phone:
+            print(f"{tab}Error: Contact's Phone number to be removed cannot be empty.{new_line}")
+            return
+
+        for n, note in enumerate(notes):
+            if note.surname == contact_surname.capitalize():
+                for p, phone in enumerate(note.phones):
+                    if phone == contact_phone:
+                        note.phones.remove(contact_phone)
 
     def add_email(file_name, delimiter=DELIMITER, encoding=ENCODING):
-        pass
+
+        contact_surname = input(f"{tab}Input a Surname of a Contact:{new_line}")
+        if not contact_surname:
+            print(f"{tab}Error: Contact's Surname cannot be empty.{new_line}")
+            return
+
+        email = input(f"{tab}Input an E-mail address of a Contact:{new_line}")
+        if not email:
+            print(f"{tab}Error: Contact's E-mail address cannot be empty.{new_line}")
+            return
+
+        for n, note in enumerate(notes):
+            if note.surname == contact_surname.capitalize():
+                pupil = Contact(
+                    name=note.name,
+                    surname=note.surname,
+                    phones=note.phones,
+                    emails=note.emails
+                )
+
+                pupil.emails.append(email)
 
     def remove_email(file_name, delimiter=DELIMITER, encoding=ENCODING):
-        pass
+
+        contact_surname = input(f"{tab}Input the Surname of a Contact:{new_line}")
+        if not contact_surname:
+            print(f"{tab}Error: Contact's Surname cannot be empty.{new_line}")
+            return
+
+        contact_email = input(f"{tab}Input an E-mail address of a Contact to be removed:{new_line}")
+        if not contact_email:
+            print(f"{tab}Error: Contact's E-mail address to be removed cannot be empty.{new_line}")
+            return
+
+        for n, note in enumerate(notes):
+            if note.surname == contact_surname.capitalize():
+                for e, email in enumerate(note.emails):
+                    if email == contact_email:
+                        note.emails.remove(contact_email)
 
     menu_prompt = f"""
             Cwiczenie 85:
